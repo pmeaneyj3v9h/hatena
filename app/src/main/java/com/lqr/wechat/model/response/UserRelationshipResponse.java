85bc1ac94fa50c4e304a4e89b7579271f69de826
pmeaneyj3v9h <wangxiao2102@yeah.net>
2021-08-24 17:35:07 +0800

    小修改

--- app/src/main/java/com/lqr/wechat/model/response/UserRelationshipResponse.java
+++ app/src/main/java/com/lqr/wechat/model/response/UserRelationshipResponse.java
@@ -1,5 +1,7 @@
 package com.lqr.wechat.model.response;
 
+import com.lqr.wechat.util.PinyinUtils;
+
 import java.util.List;
 
 /**
@@ -8,140 +10,148 @@ import java.util.List;
  */
 public class UserRelationshipResponse {
 
-
     /**
      * code : 200
-     * result : [{"displayName":"linqiarui2","message":"","status":20,"updatedAt":"2017-04-02T05:06:15.000Z","user":{"id":"2RhVU9aR1","nickname":"linqiarui","region":"86","phone":"15622364316","portraitUri":"http://7xogjk.com1.z0.glb.clouddn.com/Fo7tMdbqSyl5t3QC4Xx9tEQowRdG"}},{"displayName":"","message":"","status":20,"updatedAt":"2017-04-15T03:35:39.000Z","user":{"id":"jJPZIQNJn","nickname":"小嚎哈","region":"86","phone":"13128896442","portraitUri":""}},{"displayName":"","message":"","status":20,"updatedAt":"2017-04-16T05:42:53.000Z","user":{"id":"nmblERhmT","nickname":"合理","region":"86","phone":"18577494191","portraitUri":""}},{"displayName":"","message":"已经start啦","status":20,"updatedAt":"2017-04-17T09:21:50.000Z","user":{"id":"7MGuZVHMI","nickname":"FynnJason","region":"86","phone":"13689619679","portraitUri":"http://7xogjk.com1.z0.glb.clouddn.com/Fl7fwZhserZIkuMAjBp3Fcp7GYEO"}},{"displayName":"","message":"大神，我们交流一下","status":11,"updatedAt":"2017-04-17T10:40:55.000Z","user":{"id":"sNSu362Ns","nickname":"刘伟","region":"86","phone":"18202335430","portraitUri":""}}]
+     * result : [{"displayName":"","message":"手机号:18622222222昵称:的用户请求添加你为好友","status":11,"updatedAt":"2016-01-07T06:22:55.000Z","user":{"id":"i3gRfA1ml","nickname":"nihaoa","portraitUri":""}}]
      */
 
     private int code;
-    private List<ResultEntity> result;
+    /**
+     * displayName :
+     * message : 手机号:18622222222昵称:的用户请求添加你为好友
+     * status : 11
+     * updatedAt : 2016-01-07T06:22:55.000Z
+     * user : {"id":"i3gRfA1ml","nickname":"nihaoa","portraitUri":""}
+     */
 
-    public int getCode() {
-        return code;
-    }
+    private List<ResultEntity> result;
 
     public void setCode(int code) {
         this.code = code;
-    }
-
-    public List<ResultEntity> getResult() {
-        return result;
     }
 
     public void setResult(List<ResultEntity> result) {
         this.result = result;
     }
 
-    public static class ResultEntity {
-        /**
-         * displayName : linqiarui2
-         * message :
-         * status : 20
-         * updatedAt : 2017-04-02T05:06:15.000Z
-         * user : {"id":"2RhVU9aR1","nickname":"linqiarui","region":"86","phone":"15622364316","portraitUri":"http://7xogjk.com1.z0.glb.clouddn.com/Fo7tMdbqSyl5t3QC4Xx9tEQowRdG"}
-         */
+    public int getCode() {
+        return code;
+    }
+
+    public List<ResultEntity> getResult() {
+        return result;
+    }
+
+    public static class ResultEntity implements Comparable {
+
+        public ResultEntity(String displayName, String message, int status, String updatedAt, UserEntity user) {
+            this.displayName = displayName;
+            this.message = message;
+            this.status = status;
+            this.updatedAt = updatedAt;
+            this.user = user;
+            this.mPinyin = PinyinUtils.getPinyin(displayName);
+        }
+
+        public ResultEntity() {
+
+        }
 
         private String displayName;
         private String message;
         private int status;
         private String updatedAt;
-        private UserEntity user;
+        private String mPinyin;
+        /**
+         * id : i3gRfA1ml
+         * nickname : nihaoa
+         * portraitUri :
+         */
 
-        public String getDisplayName() {
-            return displayName;
-        }
+        private UserEntity user;
 
         public void setDisplayName(String displayName) {
             this.displayName = displayName;
-        }
-
-        public String getMessage() {
-            return message;
         }
 
         public void setMessage(String message) {
             this.message = message;
         }
 
-        public int getStatus() {
-            return status;
-        }
-
         public void setStatus(int status) {
             this.status = status;
-        }
-
-        public String getUpdatedAt() {
-            return updatedAt;
         }
 
         public void setUpdatedAt(String updatedAt) {
             this.updatedAt = updatedAt;
         }
 
-        public UserEntity getUser() {
-            return user;
-        }
-
         public void setUser(UserEntity user) {
             this.user = user;
         }
 
-        public static class UserEntity {
-            /**
-             * id : 2RhVU9aR1
-             * nickname : linqiarui
-             * region : 86
-             * phone : 15622364316
-             * portraitUri : http://7xogjk.com1.z0.glb.clouddn.com/Fo7tMdbqSyl5t3QC4Xx9tEQowRdG
-             */
+        public String getDisplayName() {
+            return displayName;
+        }
 
+        public String getMessage() {
+            return message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public String getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public UserEntity getUser() {
+            return user;
+        }
+
+        public String getPinyin() {
+            return mPinyin;
+        }
+
+        public void setPinyin(String pinyin) {
+            mPinyin = pinyin;
+        }
+
+        @Override
+        public int compareTo(Object another) {
+            return 0;
+        }
+
+        public static class UserEntity {
             private String id;
             private String nickname;
-            private String region;
-            private String phone;
             private String portraitUri;
-
-            public String getId() {
-                return id;
-            }
 
             public void setId(String id) {
                 this.id = id;
-            }
-
-            public String getNickname() {
-                return nickname;
             }
 
             public void setNickname(String nickname) {
                 this.nickname = nickname;
             }
 
-            public String getRegion() {
-                return region;
+            public void setPortraitUri(String portraitUri) {
+                this.portraitUri = portraitUri;
             }
 
-            public void setRegion(String region) {
-                this.region = region;
+            public String getId() {
+                return id;
             }
 
-            public String getPhone() {
-                return phone;
-            }
-
-            public void setPhone(String phone) {
-                this.phone = phone;
+            public String getNickname() {
+                return nickname;
             }
 
             public String getPortraitUri() {
                 return portraitUri;
             }
-
-            public void setPortraitUri(String portraitUri) {
-                this.portraitUri = portraitUri;
-            }
         }
     }
 }
+
